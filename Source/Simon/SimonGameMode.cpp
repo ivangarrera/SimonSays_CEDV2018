083090ae506baa97689aa9a27a8a16ab2ab33ac7
ivangarrera <iesivangarcia@gmail.com>
2018-02-23 13:53:51 +0100

    Json file to store records

--- Source/Simon/SimonGameMode.cpp
+++ Source/Simon/SimonGameMode.cpp
@@ -2,6 +2,8 @@
 
 #include "SimonGameMode.h"
 #include "SimonPlayerController.h"
+#include "Engine.h"
+#include "Json.h"
 #include "SimonPawn.h"
 
 ASimonGameMode::ASimonGameMode() : bIsEasy(false), bIsMedium(false), bIsHard(false), bIsEndless(false)
@@ -10,4 +12,17 @@ ASimonGameMode::ASimonGameMode() : bIsEasy(false), bIsMedium(false), bIsHard(fal
 	DefaultPawnClass = ASimonPawn::StaticClass();
 	// use our own player controller class
 	PlayerControllerClass = ASimonPlayerController::StaticClass();
+}
+
+void ASimonGameMode::ReadJsonFile()
+{
+	// Read Json File into TMap
+	FString FullPath = FPaths::ProjectDir() + "Content/Data/Records.json";
+	FString JsonStr;
+	FFileHelper::LoadFileToString(JsonStr, *FullPath);
+	TSharedRef<TJsonReader<TCHAR>> JsonReader = FJsonStringReader::Create(JsonStr);
+
+	JsonObject =  MakeShareable(new FJsonObject());
+	FJsonSerializer::Deserialize(JsonReader, JsonObject);
+
 }
